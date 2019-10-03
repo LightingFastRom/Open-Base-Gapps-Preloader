0265623389136ea1f8907933e22258d287f7d7d2
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-10-03 09:58:01 -0400

    Updating opengapps.mk to better meat user need

--- opengapps.mk
+++ opengapps.mk
@@ -57,146 +57,147 @@ ifeq ($(WITH_GAPPS), true)
 
   #Add none android tv apps if PRODUCT_CHARACTERISTICS :=phone or PRODUCT_CHARACTERISTICS :=tablet
   ifeq ($(filter phone tablet,$(PRODUCT_CHARACTERISTICS)),)
-  #Extra Apps I Really Want In My Custom Rom For Phone
-  GAPPS_PRODUCT_PACKAGES += \
-    FaceLock \
-    libfilterpack_facedetect \
-    GoogleCalendarSyncAdapter \
-    GoogleContactsSyncAdapter
+    #Extra Apps I Really Want In My Custom Rom For Phone
+    GAPPS_PRODUCT_PACKAGES += \
+      FaceLock \
+      libfilterpack_facedetect \
+      GoogleCalendarSyncAdapter \
+      GoogleContactsSyncAdapter
 
-  GAPPS_PACKAGE_OVERRIDES := \
-    FaceLock \
-    libfilterpack_facedetect \
-    GoogleCalendarSyncAdapter \
-    GoogleContactsSyncAdapter \
-    PrebuiltExchange3Google
+    GAPPS_PACKAGE_OVERRIDES := \
+      FaceLock \
+      libfilterpack_facedetect \
+      GoogleCalendarSyncAdapter \
+      GoogleContactsSyncAdapter \
+      PrebuiltExchange3Google
 
   endif
+
   #Add GoogleDialer CarrierServices & PrebuiltBugle If DEVICE_HAS_DATELINE :=true
   ifeq ($(DEVICE_HAS_DATELINE), true)
     #Extra Apps I Really Want In My Custom Rom For Devices With Datalines
     GAPPS_PRODUCT_PACKAGES += \
-      Maps
+    Maps
 
     GAPPS_PACKAGE_OVERRIDES := \
-      Maps
+    Maps
   endif
   #Use mini VARIANT If SLIM_THIS_DEVICE :=true
   ifeq ($(SLIM_THIS_DEVICE), true)
-  #Add none android tv apps if PRODUCT_CHARACTERISTICS :=phone or PRODUCT_CHARACTERISTICS :=tablet
-  ifeq ($(filter phone tablet,$(PRODUCT_CHARACTERISTICS)),)
-  #Extra Apps I Really Want In My Custom Rom For Phone
-  GAPPS_PRODUCT_PACKAGES += \
-    Chrome
+    #Add none android tv apps if PRODUCT_CHARACTERISTICS :=phone or PRODUCT_CHARACTERISTICS :=tablet
+      ifeq ($(filter phone tablet,$(PRODUCT_CHARACTERISTICS)),)
+        #Extra Apps I Really Want In My Custom Rom For Phone
+        GAPPS_PRODUCT_PACKAGES += \
+          Chrome
 
-  GAPPS_PACKAGE_OVERRIDES := \
-    jelly
+        GAPPS_PACKAGE_OVERRIDES := \
+          jelly
 
-  endif
+    endif
   else
-  #Add none android tv apps if PRODUCT_CHARACTERISTICS :=phone or PRODUCT_CHARACTERISTICS :=tablet
-  ifeq ($(filter phone tablet,$(PRODUCT_CHARACTERISTICS)),)
-  #Extra Apps I Really Want In My Custom Rom For Phone
-  GAPPS_PRODUCT_PACKAGES += \
-    GoogleContacts \
-    CalculatorGoogle \
-    CalendarGooglePrebuilt \
-    PrebuiltDeskClockGoogle \
-    PrebuiltExchange3Google \
-    PrebuiltGmail
-
-  GAPPS_PACKAGE_OVERRIDES := \
-    Contacts \
-    Calculator \
-    Calendar \
-    DeskClock \
-    PrebuiltExchange3Google \
-    PrebuiltGmail
-  endif
-
-  #Add TagGoogle & Wallet If BOARD_HAVE_NFC :=true & DEVICE_SUPPORT_NFC :=true
-  ifeq ($(BOARD_HAVE_NFC), true)
-  #Add Google NFC
-    GAPPS_PRODUCT_PACKAGES += \
-      TagGoogle
-
-    GAPPS_PACKAGE_OVERRIDES := \
-      Tag
-  endif
-
-  #Add GoogleDialer CarrierServices & PrebuiltBugle If DEVICE_HAS_DATELINE :=true
-  ifeq ($(DEVICE_HAS_DATELINE), true)
-    #Extra Apps I Really Want In My Custom Rom For Devices With Datalines
-    GAPPS_PRODUCT_PACKAGES += \
-      CarrierServices \
-      PrebuiltBugle
-
-    GAPPS_PACKAGE_OVERRIDES := \
-      CarrierServices \
-      messaging
-  endif
-
-  #Add GoogleDialer If DEVICE_HAS_VOICE_CAPABILITY :=true(For Tablets only)
-  ifeq ($(DEVICE_HAS_VOICE_CAPABILITY), true)
-    #Extra Apps I Really Want In My Custom Rom For Phone
-    GAPPS_PRODUCT_PACKAGES += \
-      GoogleDialer
-
-    GAPPS_PACKAGE_OVERRIDES := \
-      Dialer
-  endif
-
-  #Add PrebuiltBugle & GoogleDialer If PRODUCT_CHARACTERISTICS :=phone
-  ifeq ($(PRODUCT_CHARACTERISTICS), phone)
-  #Extra Apps I Really Want In My Custom Rom For Phone
-    GAPPS_PRODUCT_PACKAGES += \
-      GoogleDialer
-
-    GAPPS_PACKAGE_OVERRIDES := \
-      Dialer
-
-    #Add FitnessPrebuilt & Wallet If DEVICE_IS_FOR_GIM :=true
-    ifeq ($(DEVICE_IS_FOR_GIM), true)
+    #Add none android tv apps if PRODUCT_CHARACTERISTICS :=phone or PRODUCT_CHARACTERISTICS :=tablet
+    ifeq ($(filter phone tablet,$(PRODUCT_CHARACTERISTICS)),)
+      #Extra Apps I Really Want In My Custom Rom For Phone
       GAPPS_PRODUCT_PACKAGES += \
-      FitnessPrebuilt \
-      Wallet
+        GoogleContacts \
+        CalculatorGoogle \
+        CalendarGooglePrebuilt \
+        PrebuiltDeskClockGoogle \
+        PrebuiltExchange3Google \
+        PrebuiltGmail
 
       GAPPS_PACKAGE_OVERRIDES := \
-      FitnessPrebuilt \
-      Wallet
+        Contacts \
+        Calculator \
+        Calendar \
+        DeskClock \
+        PrebuiltExchange3Google \
+        PrebuiltGmail
+    endif
+
+    #Add TagGoogle & Wallet If BOARD_HAVE_NFC :=true & DEVICE_SUPPORT_NFC :=true
+    ifeq ($(BOARD_HAVE_NFC), true)
+      #Add Google NFC
+      GAPPS_PRODUCT_PACKAGES += \
+        TagGoogle
+
+      GAPPS_PACKAGE_OVERRIDES := \
+        Tag
+    endif
+
+    #Add GoogleDialer CarrierServices & PrebuiltBugle If DEVICE_HAS_DATELINE :=true
+    ifeq ($(DEVICE_HAS_DATELINE), true)
+      #Extra Apps I Really Want In My Custom Rom For Devices With Datalines
+      GAPPS_PRODUCT_PACKAGES += \
+        CarrierServices \
+        PrebuiltBugle
+
+      GAPPS_PACKAGE_OVERRIDES := \
+        CarrierServices \
+        messaging
+    endif
+
+    #Add GoogleDialer If DEVICE_HAS_VOICE_CAPABILITY :=true(For Tablets only)
+    ifeq ($(DEVICE_HAS_VOICE_CAPABILITY), true)
+      #Extra Apps I Really Want In My Custom Rom For Phone
+      GAPPS_PRODUCT_PACKAGES += \
+        GoogleDialer
+
+      GAPPS_PACKAGE_OVERRIDES := \
+        Dialer
+    endif
+
+    #Add PrebuiltBugle & GoogleDialer If PRODUCT_CHARACTERISTICS :=phone
+    ifeq ($(PRODUCT_CHARACTERISTICS), phone)
+      #Extra Apps I Really Want In My Custom Rom For Phone
+      GAPPS_PRODUCT_PACKAGES += \
+        GoogleDialer
+
+      GAPPS_PACKAGE_OVERRIDES := \
+        Dialer
+
+    #Add FitnessPrebuilt & Wallet If DEVICE_IS_FOR_GIM :=true
+      ifeq ($(DEVICE_IS_FOR_GIM), true)
+        GAPPS_PRODUCT_PACKAGES += \
+          FitnessPrebuilt \
+          Wallet
+
+        GAPPS_PACKAGE_OVERRIDES := \
+          FitnessPrebuilt \
+          Wallet
+      endif
+    endif
+
+    #Add Drive, Maps & Anyother cool app If DEVICE_SUPPORT_DJ :=true
+    ifeq ($(DEVICE_SUPPORT_DJ), true)
+      #Open Gapps PreLoaded Please Add To Each Device
+      GAPPS_VARIANT := nano
+      #Apps I Really Want In My Custom Rom
+      GAPPS_PRODUCT_PACKAGES +=\
+        Drive \
+        Maps
+
+      # Setting
+      GAPPS_PACKAGE_OVERRIDES := \
+        Drive \
+        Maps
+
+    ifeq ($(PRODUCT_CHARACTERISTICS), phone)
+      GAPPS_EXCLUDED_PACKAGES := \
+        Maps
+    endif
+    else
+      #Open Gapps PreLoaded Please Add To Each Device
+      GAPPS_VARIANT := nano
+      #Apps I Really Want In My Custom Rom
+      GAPPS_PRODUCT_PACKAGES +=\
+        LatinImeGoogle
+
+      # Setting
+      GAPPS_PACKAGE_OVERRIDES := \
+        HackersKeyboard
     endif
   endif
-
-  #Add Drive, Maps & Anyother cool app If DEVICE_SUPPORT_DJ :=true
-  ifeq ($(DEVICE_SUPPORT_DJ), true)
-    #Open Gapps PreLoaded Please Add To Each Device
-    GAPPS_VARIANT := nano
-    #Apps I Really Want In My Custom Rom
-    GAPPS_PRODUCT_PACKAGES +=\
-      Drive \
-      Maps
-
-    # Setting
-    GAPPS_PACKAGE_OVERRIDES := \
-      Drive \
-      Maps
-
-  ifeq ($(PRODUCT_CHARACTERISTICS), phone)
-    GAPPS_EXCLUDED_PACKAGES := \
-      Maps
-  endif
-  else
-    #Open Gapps PreLoaded Please Add To Each Device
-    GAPPS_VARIANT := nano
-    #Apps I Really Want In My Custom Rom
-    GAPPS_PRODUCT_PACKAGES +=\
-    LatinImeGoogle
-
-    # Setting
-    GAPPS_PACKAGE_OVERRIDES := \
-    HackersKeyboard
-  endif
-
   $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
   GAPPS_FORCE_MATCHING_DPI := true
